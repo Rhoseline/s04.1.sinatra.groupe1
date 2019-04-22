da1956ed7c922550c065231890a98e3c0024904e
gui3 <guillaume.3.7.13@gmail.com>
2019-04-22 15:32:26 +0200

    work in progress

---
+++ lib/gossip.rb
@@ -0,0 +1,26 @@
+class Gossip
+  attr_reader :author, :content
+  def initialize (author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = CSV.read("./db/gossip.csv").map do |csv_line|
+      Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find (id)
+    row = CSV.read("./db/gossip.csv")[id]
+    Gossip.new(row[0], row[1])
+  end
+
+end
